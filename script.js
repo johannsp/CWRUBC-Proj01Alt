f4013997d7830611b0b6ec7c4155f19520e148e8
John L Spetz <JohnLSpetz@zoho.com>
2020-07-12 18:23:12 -0400

    Test 18:23 with improved apostrophe escaping

--- script.js
+++ script.js
@@ -102,7 +102,7 @@ $(document).ready(function() {
     callObj.url = queryURL;
     // Set data property to an object with property Text inside an array
     console.log('∞° phrasesAsRecorded[phraseIndex]="'+phrasesAsRecorded[phraseIndex],'"');
-    escaped = phrasesAsRecorded[phraseIndex].replace(/'/g,"\'");
+    escaped = phrasesAsRecorded[phraseIndex].replace(/'/g,"\\'");
     console.log('∞° escaped="'+escaped,'"');
     callObj.data = "[{'Text':'"+escaped+"'}]";
     // Make asynchronous API call
